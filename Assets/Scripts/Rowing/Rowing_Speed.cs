7f6e3fa5ee0fc22f4e15b9780fbdfc321db57a07
s1766594 <grant.burgess@griffithuni.edu.au>
2017-05-03 21:52:05 +1000

    Added Scripts folder

---
+++ Assets/Scripts/Rowing/Rowing_Speed.cs
@@ -0,0 +1,37 @@
+﻿/*  Author: Grant Burgess
+    Date: 03/05/17
+    Purpose: To read a signal from the ANT+ device, and convert to the speed our enviroment will move at.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ANT_Managed_Library;
+using Dynastream.Fit;
+
+
+
+public class Rowing_Speed : MonoBehaviour {
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
