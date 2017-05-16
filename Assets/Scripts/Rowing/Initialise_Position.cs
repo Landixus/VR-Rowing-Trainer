7d3650b5856d45d4f04d3e5e22848685462e2120
extremevr <extremevr2017@gmail.com>
2017-05-17 02:45:42 +1000

    Added initialise position script, added pacing boat to scene

---
+++ Assets/Scripts/Rowing/Initialise_Position.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Initialise_Position : MonoBehaviour {
+    public static Vector3 triggerPosition; //position the user presses the trigger 
+    public static SteamVR_TrackedObject trackedObject; //boat object
+    public static SteamVR_Controller.Device controller; //controller 
+    public static bool reposition; //tracks if the position has been changed
+
+	// Use this for initialization
+	void Start () {
+        trackedObject = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        controller = SteamVR_Controller.Input((int)trackedObject.index);
+        if (controller.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            changeOrigin();
+        }
+    }
+
+    //get position of the seat
+    public static Vector3 seatPosition()
+    {
+        triggerPosition = controller.transform.pos;
+        return triggerPosition;
+    }
+
+    //repostions the origin point with respect to the seat position
+    public static void changeOrigin()
+    {
+        trackedObject.transform.position = seatPosition();
+    }
+}
