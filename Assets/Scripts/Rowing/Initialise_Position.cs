188457c0d98f49cfd385910e5e7341a55faaa7e6
Idea Lab <idealabgu@gmail.com>
2017-05-19 16:33:42 +1000

    Added video to sphere

--- Assets/Scripts/Rowing/Initialise_Position.cs
+++ Assets/Scripts/Rowing/Initialise_Position.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Initialise_Position : MonoBehaviour {
-    public static Vector3 triggerPosition; //position the user presses the trigger 
+    public static Vector3 triggerPosition; //position of the controller when trigger is pressed
+	public static Vector3 triggerRotation; //rotation of the contoller
     public static SteamVR_TrackedObject trackedObject; //boat object
     public static SteamVR_Controller.Device controller; //controller 
     public static bool reposition; //tracks if the position has been changed
 
 	// Use this for initialization
 	void Start () {
-        trackedObject = GetComponent<SteamVR_TrackedObject>();
+        trackedObject = GetComponent<SteamVR_TrackedObject>().;
 	}
 	
 	// Update is called once per frame
@@ -26,6 +27,7 @@ public class Initialise_Position : MonoBehaviour {
     public static Vector3 seatPosition()
     {
         triggerPosition = controller.transform.pos;
+		triggerRotation = 
         return triggerPosition;
     }
 
