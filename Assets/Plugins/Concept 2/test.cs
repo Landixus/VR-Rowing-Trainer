8bda3f4429333efe4cb003fdbc2c126b77ec212a
s1766594 <grant.burgess@griffithuni.edu.au>
2017-05-22 00:07:52 +1000

    Added Concept 2 SDK

---
+++ Assets/Plugins/Concept 2/test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class test : MonoBehaviour {
+    [DllImport("PM3USBCP.dll")]
+    static extern uint TkcmdsetUSB_get_dll_version();
+    static extern int TkcmdsetUSB_init();
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
