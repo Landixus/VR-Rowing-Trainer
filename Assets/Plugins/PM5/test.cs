071697170d39617baeea418a8b7f09d8e25ab87f
Idea Lab <idealabgu@gmail.com>
2017-05-22 16:21:33 +1000

    added pm5 plugin, initialise pm5

---
+++ Assets/Plugins/PM5/test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+	public int err;
+
+	[DllImport("PM3DDICP.dll")]
+	//[DllImport("PM3USBCP.dll")]
+	//[DllImport("PM3CsafeCP.dll")]
+	static extern int tkcmdsetDDI_init();
+	//static extern int tkcmdsetDDI_serial_number();
+
+	// Use this for initialization
+	void Start () {
+		err = tkcmdsetDDI_init();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("PM5 Init" + err);
+	}
+}
