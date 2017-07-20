dea66f296bf22a78472601f4641fada869cbe001
extremevr <extremevr2017@gmail.com>
2017-07-21 00:59:53 +1000

    Added training summary panel
    added training summary panel, added slider value, pace boat now has
    value of slider

--- Assets/Scripts/Rowing/GetTargetSpeed.cs
+++ Assets/Scripts/Rowing/GetTargetSpeed.cs
@@ -6,10 +6,12 @@ using UnityEngine.UI;
 public class GetTargetSpeed : MonoBehaviour {
     
     public float SliderValue; //value from slider as float
+    public Text ValueText;
 
     public void TargetSpeed()
     {
         SliderValue = GameObject.Find("TargetSpeedSlider").GetComponent<Slider>().value;
         Debug.Log(SliderValue);
+        ValueText.text = "Target Speed: " + SliderValue.ToString();
     }
 }
--- Assets/Scripts/Rowing/Pace_Boat.cs
+++ Assets/Scripts/Rowing/Pace_Boat.cs
@@ -8,14 +8,19 @@ public class Pace_Boat : MonoBehaviour {
     public double playerspeed; //speed of the player boat in m/s
     public float deltaspeed; //difference in speed
     public Transform pb; //position of the pace boat
-    public GetTargetSpeed ts; //
-        
+    //public GetTargetSpeed ts; //
+    //public GameObject slider;
+
+    private void Awake()
+    {
+        pbspeed = GameObject.Find("SceneDataManager").GetComponent<GetTargetSpeed>().SliderValue;
+        Debug.Log(pbspeed);
+    }
+
 
     // Use this for initialization
     void Start () {
-        ts = GameObject.Find("TargetSpeedSlider").GetComponent<GetTargetSpeed>();
-        Debug.Log(ts.SliderValue);
-        pbspeed = ts.SliderValue;
+       
     }
 	
 	// Update is called once per frame
