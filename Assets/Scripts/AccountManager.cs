5b7cb4bf6ca1aef23e669220423424fb783031ef
extremevr <extremevr2017@gmail.com>
2017-07-24 10:59:40 +1000

    Fixed account manager script
    added error checking

--- Assets/Scripts/AccountManager.cs
+++ Assets/Scripts/AccountManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.IO;
+using System;
 
 public class AccountManager : MonoBehaviour {
 
@@ -10,22 +11,35 @@ public class AccountManager : MonoBehaviour {
     public string loggedInUser;
     public string userPath;
     
+    //checks if user exists or creates new user and logs in
     public void CheckAccount()
     {
-        //string path = "C:\\Users\\Ben\\Documents\\GitHub\\VR-Rowing-Trainer\\Assets\\Accounts\\";
-		string path = "D:\\Projects\\Student Teams\\VR-Rowing-Trainer\\Assets\\Accounts\\";
-
-
-		if (Directory.Exists(path + username.text))
+        string path = "C:\\Users\\Ben\\Documents\\GitHub\\VR-Rowing-Trainer\\Assets\\Accounts\\";
+        //string path = "D:\\Projects\\Student Teams\\VR-Rowing-Trainer\\Assets\\Accounts\\";
+        try
         {
-            loggedInUser = username.text;
-            userPath = path + loggedInUser;
+            // Determine whether the directory exists.
+            if (Directory.Exists(path + username.text))
+            {
+                loggedInUser = username.text;
+                userPath = path + loggedInUser;
+                Debug.Log("That path exists already.");
+                return;
+            }
+            else
+            {
+                // Try to create the directory.
+                Directory.CreateDirectory(path + username.text);
+                loggedInUser = username.text;
+                userPath = path + loggedInUser;
+                Debug.LogFormat("The directory was created successfully at {0}", Directory.GetCreationTime(userPath));
+            }
         }
-        else
+        catch (Exception e)
         {
-            Directory.CreateDirectory(path + username.text);
-            loggedInUser = username.text;
-            userPath = path + loggedInUser;
+            Debug.LogFormat("The process failed: {0}", e.ToString());
         }
+        finally { }
+
     }
 }
